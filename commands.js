141efc4d5c2fcb389cdfb2bee5c464be49d7596a
Guilherme Henrique <guihss.cs@gmail.com>
2023-12-31 04:26:58 +0100

    added test function

---
+++ commands.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+let commands = [
+    {
+        name: "test",
+        description: "Test server.",
+        type: 1
+    },
+    {
+        name: "mute",
+        description: "Mute all.",
+        type: 1
+    },
+    {
+        name: "unmute",
+        description: "Unmute all.",
+        type: 1
+    }
+]
+
+axios.put(`https://discord.com/api/v10/applications/${process.env.APP_ID}/commands`, commands, {
+    headers: {
+        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+        'Content-Type': 'application/json; charset=UTF-8',
+        'User-Agent': 'DiscordBot'
+    }
+}).then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
+
--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-const express = require('express')
+import express from 'express'
+import { InteractionType, InteractionResponseType } from 'discord-interactions'
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/interactions', async (req, res) => {
+    const { type, id, data } = req.body;
+
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
+    }
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+        if (name === 'test') {
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'test',
+                },
+            });
+        } else if (name === 'mute') {
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'mute',
+                },
+            });
+        } else if (name === 'unmute') {
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: 'unmute',
+                },
+            });
+        }
+    }
+
 })
 
 app.listen(port, '0.0.0.0', () => {
